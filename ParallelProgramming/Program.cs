cb8d0beebd8530456cba4e145f72887f1382ef4c
Денис Зварун <zvarunden@mail.ru>
2020-05-24 18:05:43 +0300

    fix

--- ParallelProgramming/Program.cs
+++ ParallelProgramming/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System;
-using System.Threading;
-using System.Linq;
 using System.Collections.Generic;
-using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
